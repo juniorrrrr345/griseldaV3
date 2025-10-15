afa7a4410e9f4063d45f34640bc6d2eeba2855c3
juniorrrrr345 <Sousgrosj1@gmail.com>
2025-10-15 02:11:29 +0200

    Update AdminUsers.jsx

--- src/pages/admin/AdminUsers.jsx
+++ src/pages/admin/AdminUsers.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import { motion } from 'framer-motion'
 
-const API_URL = 'https://thegd33.calitek-junior.workers.dev'
+const API_URL = 'https://calitekv3.calitek-junior.workers.dev'
 
 const AdminUsers = () => {
   const [users, setUsers] = useState([])
